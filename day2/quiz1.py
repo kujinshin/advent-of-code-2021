021fcf4684456a63f4f01ac82da857d4d4a21a26
Ku Jin Shin <kujin.shin@gmail.com>
2021-12-02 07:16:56 -0800

    Day 2: Dive!

---
+++ day2/quiz1.py
@@ -0,0 +1,24 @@
+import sys
+
+input_file = sys.argv[1]
+
+data = []
+with open(input_file, "r") as f:
+    data = [x.rstrip() for x in f.readlines()]
+
+
+horizontal_position = 0
+depth = 0
+
+for row in data:
+    command, value = row.split(" ")
+    value = int(value)
+
+    if command == "forward":
+        horizontal_position += value
+    elif command == "down":
+        depth += value
+    elif command == "up":
+        depth -= value
+
+print(horizontal_position * depth)
---
+++ day2/quiz2.py
@@ -0,0 +1,27 @@
+import sys
+
+input_file = sys.argv[1]
+
+data = []
+with open(input_file, "r") as f:
+    data = [x.rstrip() for x in f.readlines()]
+
+
+horizontal_position = 0
+depth = 0
+aim = 0
+
+for row in data:
+    command, value = row.split(" ")
+    value = int(value)
+
+    if command == "forward":
+        horizontal_position += value
+        depth += (aim * value)
+    elif command == "down":
+        aim += value
+    elif command == "up":
+        aim -= value
+
+print(horizontal_position * depth)
+
