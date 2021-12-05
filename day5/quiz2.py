89f9e8609f28b38e011dfa71eaa1f921b3cc783e
Ku Jin Shin <kujin.shin@gmail.com>
2021-12-05 07:59:42 -0800

    Day 5: Hydrothermal Venture

---
+++ day5/quiz2.py
@@ -0,0 +1,39 @@
+import re
+import sys
+
+input_file = sys.argv[1]
+
+data = []
+with open(input_file, "r") as f:
+    data = [x.rstrip() for x in f.readlines()]
+
+
+def transform_data(data):
+    x1, y1, x2, y2 = [int(x) for x in re.findall(r'\d+', data)]
+    points = []
+
+    if x1 == x2 or y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                points.append((x, y))
+
+    if abs(x1 - x2) == abs(y1 - y2):
+        slope = (y2 - y1) // (x2 - x1)
+        b = y1 - slope * x1
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            points.append((x, slope * x + b))
+
+    return points
+
+
+point_map = {}
+for points in [transform_data(x) for x in data]:
+    for point in points:
+        point_map[point] = point_map.get(point, 0) + 1
+
+overlap = 0
+for count in point_map.values():
+    if count >= 2:
+        overlap += 1
+
+print(overlap)
